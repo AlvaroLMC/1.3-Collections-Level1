b7760587b5d2c9a5bcda38d34c678dd7d45d031b
Alvaro Mendoza <alvaromendoxxa@gmail.com>
2025-04-22 13:17:35 +0200

    1.3-Collections-Level1, Exercises 1,2,3

---
+++ src/Exercise1/Main.java
@@ -0,0 +1,53 @@
+package Exercise1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Month> months = new ArrayList<>();
+
+        months.add(new Month("January"));
+        months.add(new Month("February"));
+        months.add(new Month("March"));
+        months.add(new Month("April"));
+        months.add(new Month("May"));
+        months.add(new Month("June"));
+        months.add(new Month("July"));
+        months.add(new Month("September"));
+        months.add(new Month("October"));
+        months.add(new Month("November"));
+        months.add(new Month("December"));
+
+        System.out.println(months);
+
+        months.add(7, new Month("August"));
+
+        System.out.println(months);
+
+        HashSet<Month> months1 = new HashSet<>();
+
+        months1.add(new Month("January"));
+        months1.add(new Month("February"));
+        months1.add(new Month("March"));
+        months1.add(new Month("january"));
+
+        System.out.println("for");
+
+        for (Month m : months1) {
+            System.out.println(m);
+        }
+
+        System.out.println("iterator");
+
+        Iterator<Month> iterator = months1.iterator();
+
+        while (iterator.hasNext()) {
+            Month month = iterator.next();
+            System.out.println(month);
+        }
+
+    }
+}
---
+++ src/Exercise3/Main.java
@@ -0,0 +1,61 @@
+package Exercise3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.io.FileWriter;
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String fileName = "countries.txt";
+        HashMap<String, String> countryCapitals = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 2) {
+                    String country = parts[0].trim();
+                    String capital = parts[1].trim();
+                    countryCapitals.put(country, capital);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading the file: " + e.getMessage());
+        }
+
+        System.out.println("Enter your username: ");
+        String username = scanner.nextLine();
+
+        int count = 0;
+        int score = 0;
+
+        while (count < 10) {
+            List<String> keys = new ArrayList<>(countryCapitals.keySet());
+            Random rand = new Random();
+            int randomIndex = rand.nextInt(keys.size());
+
+            String selectedCountry = keys.get(randomIndex);
+            System.out.println((count + 1) + ". Country: " + selectedCountry + " - Capital: ");
+            String enteredCapital = scanner.nextLine();
+
+            if (countryCapitals.get(selectedCountry).equalsIgnoreCase(enteredCapital)) {
+                score++;
+            }
+
+            count++;
+        }
+
+        String outputFile = "categories.txt";
+
+        try (FileWriter writer = new FileWriter(outputFile, true)) {
+            writer.write(username + " " + score + "\n");
+            System.out.println("Data saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error saving the data: " + e.getMessage());
+        }
+    }
+}
